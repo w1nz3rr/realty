687e5fc1b95e7ac248d61c86ecb7066f9fa33d6e
nnn1k <nikitos.elidu@gmail.com>
2024-04-24 20:36:28 +0300

    user and company api

--- api/modules/users/users.py
+++ api/modules/users/users.py
@@ -4,7 +4,7 @@ from api.modules.auth.jwt_token import *
 
 users = Blueprint('users', __name__, url_prefix='/api/users')
 
-@jwt_required()
+
 @users.get('/<user_id>')
 def get_user(user_id):
     userAPI = UserAPI()
